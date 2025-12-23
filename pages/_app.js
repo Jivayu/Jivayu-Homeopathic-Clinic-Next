8b17027597e1df117338c4fa97e97d1680792357
Jivayu <9782164+Jivayu@users.noreply.github.com>
2025-12-23 13:17:28 +0530

    Remove SpeedInsights component from MyApp

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import './style.css'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+
 import { GlobalProvider } from '../global-context'
 import { NextIntlProvider } from 'next-intl'
 export default function MyApp({ Component, pageProps }) {
@@ -7,7 +7,7 @@ export default function MyApp({ Component, pageProps }) {
     <NextIntlProvider messages={pageProps?.messages}>
       <GlobalProvider>
         <Component {...pageProps} />
-        <SpeedInsights/>
+        
         </GlobalProvider>
     </NextIntlProvider>
   )
